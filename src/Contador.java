997c650950b6e61488a740040368bc39446d8b94
Pedro Lyrio <pedrolyrio@ucl.br>
2025-07-20 15:25:30 -0300

    Fix: fechando o scanner

--- src/Contador.java
+++ src/Contador.java
@@ -22,29 +22,29 @@ public class Contador {
         int parametroDois = terminal.nextInt();
 
         try {
-            //chamando o método contendo a lógica de contagem
+            // chamando o método contendo a lógica de contagem
             contar(parametroUm, parametroDois);
 
         } catch (ParametrosInvalidosException exception) {
-            //imprimir a mensagem: O segundo parâmetro deve ser maior que o
+            // imprimir a mensagem: O segundo parâmetro deve ser maior que o
             System.out.println("O segundo parâmetro deve ser maior que o primeiro parametro");
         }
-
+        terminal.close();
     }
 
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
-        //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        // validar se parametroUm é MAIOR que parametroDois e lançar a exceção
         if (parametroUm > parametroDois) {
-           throw new ParametrosInvalidosException();
+            throw new ParametrosInvalidosException();
 
         } else {
             int contagem = parametroDois - parametroUm;
-            //realizar o for para imprimir os números com base na variável contagem
+            // realizar o for para imprimir os números com base na variável contagem
             for (int i = 1; i <= contagem; i++) {
                 System.out.println("Imprimindo o número: " + i);
             }
         }
 
-
     }
+
 }
